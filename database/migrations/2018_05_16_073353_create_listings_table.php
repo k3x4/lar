408f816601dbfdc36b2c2fcb7ee657366826a842
k3x4 <xkexagias@gmail.com>
2018-05-16 12:32:02 +0300

    listing model changes

--- database/migrations/2018_05_16_073353_create_listings_table.php
+++ database/migrations/2018_05_16_073353_create_listings_table.php
@@ -17,7 +17,7 @@ class CreateListingsTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('slug');
-            $table->text('content');
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
